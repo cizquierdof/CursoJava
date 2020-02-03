4b8605be99148dd3eb4e64994d9624c7f6194c7e
WEB <cizquierdof@gmail.com>
2020-02-03 12:58:46 +0100

    1ª maven app

---
+++ helloworld/src/main/java/es/taes/persona.java
@@ -0,0 +1,42 @@
+package es.taes;
+
+/**
+ * persona
+ */
+public class Persona {
+
+    private String nombre, dni;
+    private int edad;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    @Override
+    public String toString() {
+        return "persona [dni=" + dni + ", edad=" + edad + ", nombre=" + nombre + "]";
+    }
+    
+    
+
+}
